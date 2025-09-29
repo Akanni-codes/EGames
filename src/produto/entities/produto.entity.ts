cb901efe2b109e34e46cd46ef79f0fc8286ba7b8
Akanni Silva <akanni029@gmail.com>
2025-09-29 13:32:30 -0300

    Implementação da relação entre as Entidades Produto e Categoria

--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Categoria } from '../../categoria/entities/categoria.entity';
 
 @Entity({ name: 'tb_produtos' })
 export class Produto {
@@ -25,4 +26,9 @@ export class Produto {
   @IsNotEmpty()
   @Column({ length: 5000, nullable: false })
   descricao: string;
+
+  @ManyToOne(() => Categoria, (categoria) => categoria.produto, {
+    onDelete: 'CASCADE',
+  })
+  categoria: Categoria;
 }
--- src/produto/services/produto.service.ts
+++ src/produto/services/produto.service.ts
@@ -3,22 +3,31 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Produto } from '../entities/produto.entity';
 import { ILike, Repository } from 'typeorm';
 import { DeleteResult } from 'typeorm/browser';
+import { CategoriaService } from '../../categoria/services/categoria.service';
 
 @Injectable()
 export class ProdutoService {
   constructor(
     @InjectRepository(Produto)
     private produtoRepository: Repository<Produto>,
+    private categoriaService: CategoriaService,
   ) {}
 
   async findAll(): Promise<Produto[]> {
-    return await this.produtoRepository.find();
+    return await this.produtoRepository.find({
+      relations: {
+        categoria: true,
+      },
+    });
   }
 
   async findById(id: number): Promise<Produto> {
     const produto = await this.produtoRepository.findOne({
       where: {
         id,
+      },
+      relations: {
+        categoria: true,
       },
     });
     if (!produto)
@@ -32,16 +41,20 @@ export class ProdutoService {
       where: {
         nome: ILike(`%${nome}%`),
       },
+      relations: {
+        categoria: true,
+      },
     });
   }
 
   async create(produto: Produto): Promise<Produto> {
+    await this.categoriaService.findById(produto.categoria.id);
     return await this.produtoRepository.save(produto);
   }
 
   async update(produto: Produto): Promise<Produto> {
     await this.findById(produto.id);
-
+    await this.categoriaService.findById(produto.categoria.id);
     return await this.produtoRepository.save(produto);
   }
 
